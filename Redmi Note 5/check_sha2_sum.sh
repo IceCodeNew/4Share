1feee9621a16131efe642b96b0534ba777bd3169
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2018-11-13 21:37:58 +0800

    Update Redmi Note 5/check_sha2_sum.sh

--- Redmi Note 5/check_sha2_sum.sh
+++ Redmi Note 5/check_sha2_sum.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-read -rp "Where are the checksum files? "
+read -rp "Where are the checksum files?  "
 REPLY=${REPLY//\\/\/}
 root_letter=`echo ${REPLY:0:1} | tr '[:upper:]' '[:lower:]'`
 REPLY='/mnt/'$root_letter'/'${REPLY:3}
 echo "The specified dir is: $REPLY"
 unset root_letter
-
 cd $REPLY
-par=`ls -1 | grep .sha2 | tr '\n' ' '`
-sha256sum -c $par
+
+read -rp "Please specify the checksum files' name suffix:  " par
+par=`ls -1 | grep \$par$ | tr '\n' ' '`
+read -rp "Please select M/m for MD5 sums or S/s for SHA2 sums:  "
+REPLY=`echo ${REPLY:0:1} | tr '[:lower:]' '[:upper:]'`
+if [ "$REPLY"x = "S"x ]
+then
+    sha256sum -c $par
+elif [ "$REPLY"x = "M"x ]
+then
+    md5sum -c $par
+else
+    echo "Input can't be processed!"
+fi
+unset par
