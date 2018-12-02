76ccbc8f74ece8ef03725c3900b897c00d1e0eeb
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2018-12-02 14:38:21 +0800

    Update

--- DNSCrypt/GetBlackList.sh
+++ DNSCrypt/GetBlackList.sh
@@ -8,18 +8,22 @@ echo "The specified dir is: $REPLY"
 unset root_letter
 cd $REPLY
 
-wget https://download.dnscrypt.info/blacklists/domains/mybase.txt -O domain-based-blacklist.txt
-sed -i '/.*analy.*/!d' domain-based-blacklist.txt
-sed -i '/apa\.me/d' domain-based-blacklist.txt
-sed -i '1i\apa\.me' domain-based-blacklist.txt
-sed -i -e '/^analy\./d' -e '1a\analy\.\\\*' domain-based-blacklist.txt
-sed -i -e '/^analytic\./d' -e '1a\analytic\.\\\*' domain-based-blacklist.txt
-sed -i -e '/^analytics\./d' -e '1a\analytics\.\\\*' domain-based-blacklist.txt
-sed -i -e '/^analystic\./d' -e '1a\analystic\.\\\*' domain-based-blacklist.txt
-sed -i -e '/^analysis\./d' -e '1a\analysis\.\\\*' domain-based-blacklist.txt
-sed -i -e '/^analyse\./d' -e '1a\analyse\.\\\*' domain-based-blacklist.txt
-sed -i -e '/^analyze\./d' -e '1a\analyze\.\\\*' domain-based-blacklist.txt
-sed -i -e '/^analyzer\./d' -e '1a\analyzer\.\\\*' domain-based-blacklist.txt
+wget https://download.dnscrypt.info/blacklists/domains/mybase.txt
+
+sed -i '/.*analy.*/!d' mybase.txt
+sed -i '/apa\.me/d' mybase.txt
+sed -i '1i\apa\.me' mybase.txt
+sed -i -e '/^analy\./d' -e '1a\analy\.\\\*' mybase.txt
+sed -i -e '/^analytic\./d' -e '1a\analytic\.\\\*' mybase.txt
+sed -i -e '/^analytics\./d' -e '1a\analytics\.\\\*' mybase.txt
+sed -i -e '/^analystic\./d' -e '1a\analystic\.\\\*' mybase.txt
+sed -i -e '/^analysis\./d' -e '1a\analysis\.\\\*' mybase.txt
+sed -i -e '/^analyse\./d' -e '1a\analyse\.\\\*' mybase.txt
+sed -i -e '/^analyze\./d' -e '1a\analyze\.\\\*' mybase.txt
+sed -i -e '/^analyzer\./d' -e '1a\analyzer\.\\\*' mybase.txt
+
+cp mybase.txt domain-based-blacklist.txt
+
 sed -i -e '/^mobileanalytics\./d' -e '1a\mobileanalytics\.\\\*' domain-based-blacklist.txt
+sed -i -e '/^csgosteamanalyst\./d' -e '1a\csgosteamanalyst\.\\\*' domain-based-blacklist.txt
 sed -i -e '/businessrulesanalysis\.com$/d' -e '1a\businessrulesanalysis\.com' domain-based-blacklist.txt
-sed -i -e '/analytics\.edgesuite\.net$/d' -e '1a\analytics\.edgesuite\.net' domain-based-blacklist.txt
