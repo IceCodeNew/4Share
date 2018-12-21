eaeb5018eb3c4039bf44143281b542734076a433
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2018-12-21 23:22:52 +0800

    Update

---
+++ DNSCrypt/(For Git Bash) GetBlackList.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+read -rp "Where to download the rules file? "
+REPLY=${REPLY//\\/\/}
+root_letter=`echo ${REPLY:0:1} | tr '[:upper:]' '[:lower:]'`
+REPLY='/'$root_letter'/'${REPLY:3}
+echo "The specified dir is: $REPLY"
+unset root_letter
+cd $REPLY
+
+rm domain-based-blacklist.txt mybase.txt
+curl -O https://download.dnscrypt.info/blacklists/domains/mybase.txt
+
+sed -i '/.*analy.*/!d' mybase.txt
+sed -i '/apa\.me/d' mybase.txt
+sed -i '1i\apa\.me' mybase.txt
+sed -i -e '/^analy\./d' -e '1a\analy\.\\\*' mybase.txt
+sed -i -e '/^analytic\./d' -e '1a\analytic\.\\\*' mybase.txt
+sed -i -e '/^analytics\./d' -e '1a\analytics\.\\\*' mybase.txt
+sed -i -e '/^analystic\./d' -e '1a\analystic\.\\\*' mybase.txt
+sed -i -e '/^analysis\./d' -e '1a\analysis\.\\\*' mybase.txt
+sed -i -e '/^analyse\./d' -e '1a\analyse\.\\\*' mybase.txt
+sed -i -e '/^analyze\./d' -e '1a\analyze\.\\\*' mybase.txt
+sed -i -e '/^analyzer\./d' -e '1a\analyzer\.\\\*' mybase.txt
+
+cp mybase.txt domain-based-blacklist.txt
+
+sed -i -e '/^mobileanalytics\./d' -e '1a\mobileanalytics\.\\\*' domain-based-blacklist.txt
+sed -i -e '/^csgosteamanalyst\./d' -e '1a\csgosteamanalyst\.\\\*' domain-based-blacklist.txt
+sed -i -e '/businessrulesanalysis\.com$/d' -e '1a\businessrulesanalysis\.com' domain-based-blacklist.txt
