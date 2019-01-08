ab272e0a849d4f9350b39b2e7275e50f493beb11
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2019-01-09 00:00:16 +0800

    Update

--- DNSCrypt/(For Git Bash) GetBlackList.sh
+++ DNSCrypt/(For Git Bash) GetBlackList.sh
@@ -12,19 +12,22 @@ rm domain-based-blacklist.txt mybase.txt
 curl -O https://download.dnscrypt.info/blacklists/domains/mybase.txt
 
 sed -i '/.*analy.*/!d' mybase.txt
-sed -i '/apa\.me/d' mybase.txt
-sed -i '1i\apa\.me' mybase.txt
-sed -i -e '/^analy\./d' -e '1a\analy\.\\\*' mybase.txt
-sed -i -e '/^analytic\./d' -e '1a\analytic\.\\\*' mybase.txt
-sed -i -e '/^analytics\./d' -e '1a\analytics\.\\\*' mybase.txt
-sed -i -e '/^analystic\./d' -e '1a\analystic\.\\\*' mybase.txt
-sed -i -e '/^analysis\./d' -e '1a\analysis\.\\\*' mybase.txt
-sed -i -e '/^analyse\./d' -e '1a\analyse\.\\\*' mybase.txt
-sed -i -e '/^analyze\./d' -e '1a\analyze\.\\\*' mybase.txt
-sed -i -e '/^analyzer\./d' -e '1a\analyzer\.\\\*' mybase.txt
+sed -i -e '/^analy\./d' -e '1i\analy\.\\\*' mybase.txt
+sed -i -e '/^analytic\./d' -e '1i\analytic\.\\\*' mybase.txt
+sed -i -e '/^analytics\./d' -e '1i\analytics\.\\\*' mybase.txt
+sed -i -e '/^analystic\./d' -e '1i\analystic\.\\\*' mybase.txt
+sed -i -e '/^analysis\./d' -e '1i\analysis\.\\\*' mybase.txt
+sed -i -e '/^analyse\./d' -e '1i\analyse\.\\\*' mybase.txt
+sed -i -e '/^analyze\./d' -e '1i\analyze\.\\\*' mybase.txt
+sed -i -e '/^analyzer\./d' -e '1i\analyzer\.\\\*' mybase.txt
 
 cp mybase.txt domain-based-blacklist.txt
 
-sed -i -e '/^mobileanalytics\./d' -e '1a\mobileanalytics\.\\\*' domain-based-blacklist.txt
-sed -i -e '/^csgosteamanalyst\./d' -e '1a\csgosteamanalyst\.\\\*' domain-based-blacklist.txt
-sed -i -e '/businessrulesanalysis\.com$/d' -e '1a\businessrulesanalysis\.com' domain-based-blacklist.txt
+sed -i '/apa\.me/d' domain-based-blacklist.txt
+sed -i '/skkdd\.com/d' domain-based-blacklist.txt
+sed -i '1i\apa\.me' domain-based-blacklist.txt
+sed -i '1i\skkdd\.com' domain-based-blacklist.txt
+
+sed -i -e '/^mobileanalytics\./d' -e '1i\mobileanalytics\.\\\*' domain-based-blacklist.txt
+sed -i -e '/^csgosteamanalyst\./d' -e '1i\csgosteamanalyst\.\\\*' domain-based-blacklist.txt
+sed -i -e '/businessrulesanalysis\.com$/d' -e '1i\businessrulesanalysis\.com' domain-based-blacklist.txt
