0d2caecfa27fe66a53d8e6a9e4a54cec58abfd57
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2019-08-19 13:24:20 +0800

    Update

--- Proxifier/GetWhiteDomainLists/gwl.py
+++ Proxifier/GetWhiteDomainLists/gwl.py
@@ -1,8 +1,11 @@
 import os
 import re
+from multiprocessing.pool import ThreadPool
 from typing import List, Pattern
 
 import download_file
+
+urls = []
 
 download_file.download_file('https://cdn.statically.io/gh/v2ray/domain-list-community/master/data/geolocation-cn')
 
@@ -10,8 +13,9 @@ with open(r'geolocation-cn', 'r', encoding='UTF-8') as f:
     tmplist: List = f.readlines()
     for line in tmplist:
         pattern: Pattern = re.compile(r'^include:')
-        _: str = pattern.split(line)
+        _: List = pattern.split(line)
         if _[0] == '':
             os.chdir(os.path.abspath(os.path.dirname(__file__)))
-            download_file.download_file(
-                'https://cdn.statically.io/gh/v2ray/domain-list-community/master/data/' + _[1].rstrip())
+            urls.append('https://cdn.statically.io/gh/v2ray/domain-list-community/master/data/' + _[1].rstrip())
+
+ThreadPool(8).imap_unordered(download_file, urls)
