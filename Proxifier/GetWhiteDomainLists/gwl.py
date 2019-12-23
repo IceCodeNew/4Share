7e94897ac43681ff15e5eab3d818eaeba7e6fa5a
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2019-12-23 21:30:52 +0800

    Update

--- Proxifier/GetWhiteDomainLists/gwl.py
+++ Proxifier/GetWhiteDomainLists/gwl.py
@@ -8,7 +8,7 @@ import download_file
 urls = []
 if not os.path.isdir('downloaded_rules'):
     os.mkdir('downloaded_rules')
-download_file.download_file('https://cdn.statically.io/gh/v2ray/domain-list-community/master/data/geolocation-cn')
+download_file.download_file('https://raw.githubusercontent.com/v2ray/domain-list-community/master/data/geolocation-cn')
 
 file_name = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'downloaded_rules', 'geolocation-cn')
 with open(file_name, 'r', encoding='utf-8') as f:
@@ -17,7 +17,7 @@ with open(file_name, 'r', encoding='utf-8') as f:
         pattern: Pattern = re.compile(r'^include:')
         _: List = pattern.split(line)
         if _[0] == '':
-            urls.append('https://cdn.statically.io/gh/v2ray/domain-list-community/master/data/' + _[1].rstrip())
+            urls.append('https://raw.githubusercontent.com/v2ray/domain-list-community/master/data/' + _[1].rstrip())
 
 results = ThreadPool(8).imap_unordered(download_file.download_file, urls)
 for path in results:
