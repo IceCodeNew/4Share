0793175562b446338095dfb12d14379b7e6a6d39
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2020-06-03 14:21:43 +0800

    Update

--- DNSCrypt/(For Git Bash) GetBlackList.sh
+++ DNSCrypt/(For Git Bash) GetBlackList.sh
@@ -28,6 +28,8 @@ sed -E -i 's!(^https:\/\/easylist-downloads\.adblockplus\.org\/easylistchina\.tx
 sed -E -i 's!(^https:\/\/easylist-downloads\.adblockplus\.org\/easylist_noelemhide\.txt)!# \1!' domains-blacklist.conf
 sed -E -i 's!(^https:\/\/adguardteam\.github\.io\/AdGuardSDNSFilter\/Filters\/filter\.txt)!# \1!' domains-blacklist.conf
 # sed -E -i 's!(^https:\/\/ssl\.bblck\.me\/blacklists\/domain-list\.txt)!# \1!' domains-blacklist.conf
+sed -E -i 's!(^https:\/\/isc\.sans\.edu\/feeds\/suspiciousdomains_High\.txt)!# \1!' domains-blacklist.conf
+sed -E -i 's!(^https:\/\/raw\.githubusercontent\.com\/AdAway\/adaway\.github\.io\/master\/hosts\.txt)!# \1!' domains-blacklist.conf
 
 # Append ```os._exit(0)``` and ```import os```
 python -i generate-domains-blacklist.py > mybase.txt
@@ -48,6 +50,8 @@ sed -E -i 's!^# (https:\/\/mirror1\.malwaredomains\.com\/files\/justdomains)!\1!
 # sed -E -i 's!^# (https:\/\/easylist-downloads\.adblockplus\.org\/easylist_noelemhide\.txt)!\1!' domains-blacklist.conf
 # sed -E -i 's!^# (https:\/\/adguardteam\.github\.io\/AdGuardSDNSFilter\/Filters\/filter\.txt)!\1!' domains-blacklist.conf
 sed -E -i 's!^# (https:\/\/ssl\.bblck\.me\/blacklists\/domain-list\.txt)!\1!' domains-blacklist.conf
+sed -E -i 's!^# (https:\/\/isc\.sans\.edu\/feeds\/suspiciousdomains_High\.txt)!\1!' domains-blacklist.conf
+sed -E -i 's!^# (https:\/\/raw\.githubusercontent\.com\/AdAway\/adaway\.github\.io\/master\/hosts\.txt)!\1!' domains-blacklist.conf
 
 python -i generate-domains-blacklist.py > domain-based-blacklist.txt
 
@@ -55,22 +59,31 @@ python -i generate-domains-blacklist.py > domain-based-blacklist.txt
 
 /bin/mv -f mybase.txt domain-based-blacklist.txt "$(dirname "$0")"
 cd "$(dirname "$0")"
-/bin/rm -f '../v2rayN/v2rayN_block_rules.txt' '../v2rayN/Advertising.list' '../v2rayN/Hijacking.list'
+/bin/rm -f '../v2rayN/v2rayN_block_rules.txt' '../v2rayN/custom_blacklist.txt'
 
 dos2unix fws.py ./*.txt
 winpty "$(which python)" fws.py
 
 /bin/mv -f v2rayN_block_rules.txt '../v2rayN'
 cd '../v2rayN'
-curl -o Advertising.list 'https://raw.githubusercontent.com/ConnersHua/Profiles/master/Quantumult/X/Filter/Advertising.list'
-curl -o Hijacking.list 'https://raw.githubusercontent.com/ConnersHua/Profiles/master/Quantumult/X/Filter/Hijacking.list'
-sed -E -i -e '/^#|^$/d' -e '/^DOMAIN-KEYWORD.*/d' -e '/^DOMAIN/!d' -e '/REJECT$/!d' Advertising.list Hijacking.list
-sed -E -i -e 's/REJECT$//' -e 's/^DOMAIN,/full:/' -e 's/^DOMAIN-SUFFIX,/domain:/' Advertising.list Hijacking.list
+curl -o AdBlock.list.new 'https://raw.githubusercontent.com/GeQ1an/Rules/master/QuantumultX/Filter/AdBlock.list' && sed -E -i 's/^HOST/DOMAIN/g' AdBlock.list.new
+curl -o Advertising.list.new 'https://raw.githubusercontent.com/ConnersHua/Profiles/master/Quantumult/X/Filter/Advertising.list'
+curl -o Hijacking.list.new 'https://raw.githubusercontent.com/ConnersHua/Profiles/master/Quantumult/X/Filter/Hijacking.list'
+curl -o Reject.list.new 'https://raw.githubusercontent.com/lhie1/Rules/master/Surge/Surge%203/Provider/Reject.list'
+mv AdBlock.list.new AdBlock.list;
+mv Advertising.list.new Advertising.list;
+mv Hijacking.list.new Hijacking.list;
+mv Reject.list.new Reject.list;
+sed -E -i -e '/^#|^$/d' -e '/^DOMAIN-KEYWORD.*/d' -e '/^DOMAIN/!d' -e '/REJECT$/!d' AdBlock.list Advertising.list Hijacking.list Reject.list
+sed -E -i -e 's/REJECT$//g' -e 's/^DOMAIN,/full:/g' -e 's/^DOMAIN-SUFFIX,/domain:/g' AdBlock.list Advertising.list Hijacking.list Reject.list
 
 if [ ! -f 'ori_BlackList.txt' ]; then
     rm -rf -- 'ori_BlackList.txt'
     touch 'ori_BlackList.txt'
 fi
-cat ori_BlackList.txt v2rayN_block_rules.txt Advertising.list Hijacking.list | sort | uniq > temp_v2rayN_block_rules.txt
+cat ori_BlackList.txt AdBlock.list Advertising.list Hijacking.list Reject.list | sort | uniq > custom_blacklist.txt
+cat v2rayN_block_rules.txt custom_blacklist.txt | sort | uniq > temp_v2rayN_block_rules.txt
 /bin/mv -f temp_v2rayN_block_rules.txt v2rayN_block_rules.txt
+
 dos2unix ./*.txt
+/bin/mv -f custom_blacklist.txt "$REPOS_ROOT/dnscrypt-proxy/utils/generate-domains-blacklists/custom_blacklist.txt"
