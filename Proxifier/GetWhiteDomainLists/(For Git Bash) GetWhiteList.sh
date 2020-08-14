e284b16fb96fef9d6416b018ff722c22cd298ba4
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2020-08-15 03:03:25 +0800

    Update `GetWhiteList.sh.auto`

--- Proxifier/GetWhiteDomainLists/(For Git Bash) GetWhiteList.sh
+++ Proxifier/GetWhiteDomainLists/(For Git Bash) GetWhiteList.sh
@@ -13,32 +13,36 @@ set -x
 cd "$(dirname "$0")" || exit
 rm -r 'downloaded_rules/' 'extra_dlc_cn.txt' 'whitelist.txt' 'scholar_not_cn.txt' 'tmp_whitelist.txt' 'tmp_scholar_not_cn.txt'
 
-winpty "$(which python)" 'start_yield.py' 'geolocation-cn' 'category-scholar-!cn'
+# winpty "$(which python)" 'start_yield.py' 'geolocation-cn' 'category-scholar-!cn'
+curl -LR https://raw.githubusercontent.com/IceCodeNew/domain-list-community/release/geolocation-cn.txt -o 'tmp_whitelist.txt'
+curl -LR https://raw.githubusercontent.com/IceCodeNew/domain-list-community/release/category-scholar-!cn.txt -o 'tmp_scholar_not_cn.txt'
 find . -type f -print0 | xargs -0 dos2unix
 
-(
-cd 'downloaded_rules' || exit
-while :
-do
-    find 'geolocation-cn.d' -maxdepth 1 -type f -print0 | xargs -0 sed -E -e '/^#|^$/d' -e 's/[\t ]*#[^\r\n]*//g' | winpty "$(which python)" '../deep_diger.py' 'geolocation-cn'
-    [[ ! -d 'geolocation-cn.d' ]] && break
-done
-sed -i -E -e 's/[\t ]*[#@][^\r\n]*//g' -e '/^#|^$|^\*\.$/d' 'geolocation-cn'
-sed -i -E -e '/\.googleapis\.|\.gstatic\.|\.recaptcha\.|\.ampproject\./d' 'geolocation-cn'
-mv 'geolocation-cn' '../tmp_whitelist.txt'
-
-while :
-do
-    rm 'category-scholar-!cn.d/google-scholar'
-    find 'category-scholar-!cn.d' -maxdepth 1 -type f -print0 | xargs -0 sed -E -e '/^#|^$/d' -e 's/[\t ]*#[^\r\n]*//g' | winpty "$(which python)" '../deep_diger.py' 'category-scholar-!cn'
-    [[ ! -d 'geolocation-cn.d' ]] && break
-done
-sed -i -E -e 's/[\t ]*[#@][^\r\n]*//g' -e '/^#|^$|^\*\.$/d' 'category-scholar-!cn'
-mv 'category-scholar-!cn' '../tmp_scholar_not_cn.txt'
-)
+# (
+# cd 'downloaded_rules' || exit
+# while :
+# do
+#     find 'geolocation-cn.d' -maxdepth 1 -type f -print0 | xargs -0 sed -E -e '/^#|^$/d' -e 's/[\t ]*#[^\r\n]*//g' | winpty "$(which python)" '../deep_diger.py' 'geolocation-cn'
+#     [[ ! -d 'geolocation-cn.d' ]] && break
+# done
+# sed -i -E -e 's/[\t ]*[#@][^\r\n]*//g' -e '/^#|^$|^\*\.$/d' 'geolocation-cn'
+# sed -i -E -e '/\.googleapis\.|\.gstatic\.|\.recaptcha\.|\.ampproject\./d' 'geolocation-cn'
+# mv 'geolocation-cn' '../tmp_whitelist.txt'
+# 
+# while :
+# do
+#     rm 'category-scholar-!cn.d/google-scholar'
+#     find 'category-scholar-!cn.d' -maxdepth 1 -type f -print0 | xargs -0 sed -E -e '/^#|^$/d' -e 's/[\t ]*#[^\r\n]*//g' | winpty "$(which python)" '../deep_diger.py' 'category-scholar-!cn'
+#     [[ ! -d 'geolocation-cn.d' ]] && break
+# done
+# sed -i -E -e 's/[\t ]*[#@][^\r\n]*//g' -e '/^#|^$|^\*\.$/d' 'category-scholar-!cn'
+# mv 'category-scholar-!cn' '../tmp_scholar_not_cn.txt'
+# )
 
 [[ -f 'ori_white_domains.txt' ]] && cat 'ori_white_domains.txt' >> 'tmp_whitelist.txt' \
-&& sed -E 's/\*\./domain:/g' 'ori_white_domains.txt' > 'extra_dlc_cn.txt'
+&& sed -E -e 's/\*\./domain:/' -e 's/^/full:/' -e 's/full:domain:/domain:/' 'ori_white_domains.txt' > 'extra_dlc_cn.txt'
+
+sed -E -i -e '/[-@]ads$|^regex:/d' -e 's/full://' -e 's/domain:/\*\./' 'tmp_whitelist.txt' 'tmp_scholar_not_cn.txt'
 perl -ni -e 'print unless /(?<!^\*)\.(baidu|citic|cn|sohu|unicom|xn--1qqw23a|xn--6frz82g|xn--8y0a063a|xn--estv75g|xn--fiq64b|xn--fiqs8s|xn--fiqz9s|xn--vuq861b|xn--xhq521b|xn--zfr164b)$/' 'tmp_whitelist.txt'
 sed -E -i '/^[\t\f\v ]*$/d' 'tmp_whitelist.txt' 'tmp_scholar_not_cn.txt'
 < 'tmp_whitelist.txt' sort | uniq > 'whitelist.txt'
