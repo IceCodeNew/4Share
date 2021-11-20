fe04ea84ea7906abc34a4e70f266e4a9d408c516
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2021-11-20 22:06:03 +0800

    Auto Commit

--- router/route.sh
+++ router/route.sh
@@ -718,7 +718,6 @@ route ${OPS} -net 67.198.189.0/24 ${ROUTE_GW}
 route ${OPS} -net 67.198.192.0/23 ${ROUTE_GW}
 route ${OPS} -net 67.198.195.0/24 ${ROUTE_GW}
 route ${OPS} -net 67.198.199.0/24 ${ROUTE_GW}
-route ${OPS} -net 67.198.201.0/24 ${ROUTE_GW}
 route ${OPS} -net 67.198.207.0/24 ${ROUTE_GW}
 route ${OPS} -net 67.198.208.0/24 ${ROUTE_GW}
 route ${OPS} -net 67.198.221.0/24 ${ROUTE_GW}
@@ -1235,6 +1234,7 @@ route ${OPS} -net 103.122.48.0/22 ${ROUTE_GW}
 route ${OPS} -net 103.122.192.0/24 ${ROUTE_GW}
 route ${OPS} -net 103.122.246.0/23 ${ROUTE_GW}
 route ${OPS} -net 103.123.88.0/22 ${ROUTE_GW}
+route ${OPS} -net 103.126.1.0/24 ${ROUTE_GW}
 route ${OPS} -net 103.126.124.0/22 ${ROUTE_GW}
 route ${OPS} -net 103.127.125.0/24 ${ROUTE_GW}
 route ${OPS} -net 103.127.126.0/24 ${ROUTE_GW}
