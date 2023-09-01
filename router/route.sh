064f7ab4d0ccfd57e47545ffb4f087fbd37e4fcf
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2023-09-01 23:12:05 +0800

    Auto Commit

--- router/route.sh
+++ router/route.sh
@@ -375,9 +375,10 @@ route ${OPS} -net 43.254.168.0/22 ${ROUTE_GW}
 route ${OPS} -net 43.254.192.0/22 ${ROUTE_GW}
 route ${OPS} -net 43.254.200.0/22 ${ROUTE_GW}
 route ${OPS} -net 43.254.220.0/22 ${ROUTE_GW}
-route ${OPS} -net 43.254.224.0/22 ${ROUTE_GW}
-route ${OPS} -net 43.254.232.0/21 ${ROUTE_GW}
-route ${OPS} -net 43.255.4.0/22 ${ROUTE_GW}
+route ${OPS} -net 43.254.224.0/20 ${ROUTE_GW}
+route ${OPS} -net 43.254.240.0/22 ${ROUTE_GW}
+route ${OPS} -net 43.254.248.0/22 ${ROUTE_GW}
+route ${OPS} -net 43.255.0.0/21 ${ROUTE_GW}
 route ${OPS} -net 43.255.28.0/23 ${ROUTE_GW}
 route ${OPS} -net 43.255.31.0/24 ${ROUTE_GW}
 route ${OPS} -net 43.255.68.0/22 ${ROUTE_GW}
@@ -1366,6 +1367,9 @@ route ${OPS} -net 103.228.228.0/23 ${ROUTE_GW}
 route ${OPS} -net 103.228.230.0/24 ${ROUTE_GW}
 route ${OPS} -net 103.229.148.0/22 ${ROUTE_GW}
 route ${OPS} -net 103.229.212.0/22 ${ROUTE_GW}
+route ${OPS} -net 103.229.217.0/24 ${ROUTE_GW}
+route ${OPS} -net 103.229.218.0/24 ${ROUTE_GW}
+route ${OPS} -net 103.229.236.0/22 ${ROUTE_GW}
 route ${OPS} -net 103.230.212.0/22 ${ROUTE_GW}
 route ${OPS} -net 103.230.236.0/22 ${ROUTE_GW}
 route ${OPS} -net 103.230.240.0/22 ${ROUTE_GW}
@@ -2475,7 +2479,6 @@ route ${OPS} -net 129.226.196.0/22 ${ROUTE_GW}
 route ${OPS} -net 129.226.200.0/21 ${ROUTE_GW}
 route ${OPS} -net 129.226.208.0/20 ${ROUTE_GW}
 route ${OPS} -net 130.36.146.0/23 ${ROUTE_GW}
-route ${OPS} -net 131.228.96.0/24 ${ROUTE_GW}
 route ${OPS} -net 132.145.80.0/20 ${ROUTE_GW}
 route ${OPS} -net 132.232.0.0/16 ${ROUTE_GW}
 route ${OPS} -net 132.237.134.0/24 ${ROUTE_GW}
@@ -3638,7 +3641,6 @@ route ${OPS} -net 218.98.96.0/21 ${ROUTE_GW}
 route ${OPS} -net 218.98.104.0/22 ${ROUTE_GW}
 route ${OPS} -net 218.98.108.0/23 ${ROUTE_GW}
 route ${OPS} -net 218.98.111.0/24 ${ROUTE_GW}
-route ${OPS} -net 218.98.112.0/22 ${ROUTE_GW}
 route ${OPS} -net 218.98.160.0/24 ${ROUTE_GW}
 route ${OPS} -net 218.104.0.0/15 ${ROUTE_GW}
 route ${OPS} -net 218.106.0.0/16 ${ROUTE_GW}
