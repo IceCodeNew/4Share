929e3be4dfd0ddfdb9285922a30452600281edd7
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2025-02-26 05:06:03 +0000

    Auto Commit

--- router/route.sh
+++ router/route.sh
@@ -194,7 +194,6 @@ route ${OPS} -net 36.255.164.0/24 ${ROUTE_GW}
 route ${OPS} -net 37.123.192.0/24 ${ROUTE_GW}
 route ${OPS} -net 37.123.194.0/24 ${ROUTE_GW}
 route ${OPS} -net 37.123.197.0/24 ${ROUTE_GW}
-route ${OPS} -net 38.12.36.0/23 ${ROUTE_GW}
 route ${OPS} -net 38.12.39.0/24 ${ROUTE_GW}
 route ${OPS} -net 38.46.5.0/24 ${ROUTE_GW}
 route ${OPS} -net 38.46.6.0/24 ${ROUTE_GW}
@@ -1466,7 +1465,7 @@ route ${OPS} -net 103.142.154.0/23 ${ROUTE_GW}
 route ${OPS} -net 103.142.234.0/23 ${ROUTE_GW}
 route ${OPS} -net 103.143.16.0/22 ${ROUTE_GW}
 route ${OPS} -net 103.143.92.0/23 ${ROUTE_GW}
-route ${OPS} -net 103.143.231.0/24 ${ROUTE_GW}
+route ${OPS} -net 103.143.230.0/23 ${ROUTE_GW}
 route ${OPS} -net 103.144.66.0/23 ${ROUTE_GW}
 route ${OPS} -net 103.144.70.0/24 ${ROUTE_GW}
 route ${OPS} -net 103.144.158.0/23 ${ROUTE_GW}
@@ -3218,7 +3217,7 @@ route ${OPS} -net 156.227.16.0/22 ${ROUTE_GW}
 route ${OPS} -net 156.227.24.0/22 ${ROUTE_GW}
 route ${OPS} -net 156.229.8.0/21 ${ROUTE_GW}
 route ${OPS} -net 156.232.9.0/24 ${ROUTE_GW}
-route ${OPS} -net 156.232.10.0/24 ${ROUTE_GW}
+route ${OPS} -net 156.232.10.0/23 ${ROUTE_GW}
 route ${OPS} -net 156.235.1.0/24 ${ROUTE_GW}
 route ${OPS} -net 156.235.2.0/23 ${ROUTE_GW}
 route ${OPS} -net 156.235.4.0/22 ${ROUTE_GW}
